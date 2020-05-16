98b13a104bb961fd38e236553ecd997c38520d22
ibokov <bokov@uw.edu>
2020-05-15 23:07:02 -0700

    Commiting for Revert DOn't use

--- app/src/main/java/edu/tacoma/uw/mtchea/overliftapp/SendNotificationActivity.java
+++ app/src/main/java/edu/tacoma/uw/mtchea/overliftapp/SendNotificationActivity.java
@@ -1,13 +1,18 @@
 package edu.tacoma.uw.mtchea.overliftapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.wifi.hotspot2.pps.Credential;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
 
@@ -54,6 +59,47 @@ public class SendNotificationActivity extends AppCompatActivity {
                 sendNotification(user);
             }
         });
+
+        // The navigation bar that lists all the tabs on the bottom.
+//        final BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+////        bottomNavigationView.setSelectedItemId(R.id.page_4);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.page_1:
+//                        //Toast.makeText(SocialNotification.this, "exercises", Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(SendNotificationActivity.this
+//                                , ExerciseListActivity.class);
+//                        startActivity(intent);
+//                        break;
+//                    case R.id.page_2:
+//                        //Toast.makeText(SocialNotification.this, "health", Toast.LENGTH_SHORT).show();
+//                        Intent intent2 = new Intent(SendNotificationActivity.this
+//                                , HealthActivity.class);
+//                        startActivity(intent2);
+//                        break;
+//                    case R.id.page_3:
+//                        //Toast.makeText(SocialNotification.this, "workout", Toast.LENGTH_SHORT).show();
+//                        //Context context = bottomNavigationView.getContext();
+//                        Intent intent3 = new Intent(SendNotificationActivity.this
+//                                , MainActivity.class);
+//                        startActivity(intent3);
+//                        break;
+//                    case R.id.page_4:
+//                        //Toast.makeText(SocialNotification.this, "social", Toast.LENGTH_SHORT).show();
+//                        Intent intent4 = new Intent(SendNotificationActivity.this, SocialNotification.class);
+//                        startActivity(intent4);
+//                        break;
+//                    case R.id.page_5:
+//                        //Toast.makeText(SocialNotification.this, "profile", Toast.LENGTH_SHORT).show();
+//                        Intent intent5 = new Intent(SendNotificationActivity.this, ProfileActivity.class);
+//                        startActivity(intent5);
+//                        break;
+//                }
+//                return true;
+//            }
+//        });
 
     }
 
